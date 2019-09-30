cc645365453c2e1a02850c82bd9f4274e77f3fbd
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-09-30 09:09:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ end
 
 def remove_element_from_end_of_array(array)
  array = ["I", "am", "really", "learnng", "arrays!"]
-  array.pop(1)
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array) 
