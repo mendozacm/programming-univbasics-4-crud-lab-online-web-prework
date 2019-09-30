7717cafce148cca8f74a77d397d89c322aef1431
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-09-30 09:51:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,8 +26,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  ["i", "am", "so", "tired"]
-  
+  a = ["i", "am", "so", "tired"]
+  a
 end
 
 def retrieve_first_element_from_array(array)
