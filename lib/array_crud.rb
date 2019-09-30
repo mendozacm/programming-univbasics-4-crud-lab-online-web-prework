d986af840f9afe3edc59b276662b1305cadaa21c
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-09-30 09:56:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  a = ["i", "am", "so", "tired"]
+  a = ["wow", "am", "so", "tired"]
   a[0]
 end
 
