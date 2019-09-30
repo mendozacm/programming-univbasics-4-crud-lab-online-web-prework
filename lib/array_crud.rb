f23beb1465d1021aaae2753b6f640bb38a8361bc
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-09-30 08:46:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,11 +16,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  [element, array]
-  .pop
+ array = [element, array]
+  array.pop
 end
 
-def remove_element_from_start_of_array(array)
+def remove_element_from_start_of_array(array) 
   
 end
 
