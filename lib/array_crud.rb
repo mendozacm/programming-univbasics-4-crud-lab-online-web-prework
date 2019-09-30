5ac2fcb9ef6188276abf393d185b9641c148afb9
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-09-30 08:48:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,8 +15,8 @@ def add_element_to_start_of_array(array, element)
   .unshift(element)
 end
 
-def remove_element_from_end_of_array(element, array)
- array = [element, array]
+def remove_element_from_end_of_array(element, car, array)
+ array = [element, car, array]
   array.pop
 end
 
